29dd283367557efd2546616da6fdc8db01678464
bondac-ion <70916383+bondac-ion@users.noreply.github.com>
2021-10-14 17:00:34 +0300

    Update prob3.py

--- prob3.py
+++ prob3.py
@@ -1,5 +1,4 @@
 nume=str(input("Ce nume si preume ai? -"))
 profesie=str(input("Ce iti doresti sa devii in viitor? -"))
-n=nume.title()
 print('Te numesti %s \n'
 'Doresti sa devii %s'%(n,profesie))
