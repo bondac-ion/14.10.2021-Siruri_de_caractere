c10553150f94d0de78b784e435f214cee02a96b3
bondac-ion <70916383+bondac-ion@users.noreply.github.com>
2021-10-14 16:59:58 +0300

    Add files via upload

---
+++ prob3.py
@@ -0,0 +1,5 @@
+nume=str(input("Ce nume si preume ai? -"))
+profesie=str(input("Ce iti doresti sa devii in viitor? -"))
+n=nume.title()
+print('Te numesti %s \n'
+'Doresti sa devii %s'%(n,profesie))
